2560ba0d55a57468b9f2cd3f36a3ab5a87df69fb
Austin Buddington <austin.j.buddington@gmail.com>
2022-01-15 12:53:54 -0500

    create db tool

---
+++ src/lib.rs
@@ -0,0 +1,133 @@
+//! # lmdb_utils
+//! This is a command-line utility for Casper Node operators.
+//!
+pub mod error {
+    use thiserror::Error;
+
+    #[derive(Debug, Error)]
+    pub enum ToolError {
+        #[error("failed database operation")]
+        Database(#[from] lmdb::Error),
+    }
+}
+
+mod block {
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Debug, Serialize, Deserialize, Hash)]
+    pub struct BlockHeader {
+        parent_hash: Vec<u8>,
+        state_root_hash: Vec<u8>,
+        pub body_hash: Vec<u8>,
+        random_bit: bool,
+        accumulated_seed: Vec<u8>,
+        era_end: Option<String>,
+        timestamp: u64,
+        era_id: u64,
+        height: u64,
+        protocol_version: [u32; 3],
+    }
+
+    impl BlockHeader {
+        pub fn body_hash(&self) -> String {
+            base16::encode_lower(&self.body_hash)
+        }
+
+        pub fn height(&self) -> u64 {
+            self.height
+        }
+    }
+}
+
+pub mod db {
+    use std::{collections::HashSet, path::PathBuf};
+
+    use lmdb::{
+        Cursor, Database, DatabaseFlags, Environment, EnvironmentFlags, Transaction, WriteFlags,
+    };
+
+    use super::{block::BlockHeader, error::ToolError};
+
+    type Hashes = HashSet<Vec<u8>>;
+
+    pub fn get_env(path: PathBuf) -> Result<Environment, lmdb::Error> {
+        Environment::new()
+            .set_flags(
+                EnvironmentFlags::NO_SUB_DIR
+                    | EnvironmentFlags::NO_TLS
+                    | EnvironmentFlags::NO_READAHEAD,
+            )
+            .set_max_dbs(3)
+            .open(&path)
+    }
+
+    pub fn run(db_path: PathBuf, block_height: u64) -> Result<(), ToolError> {
+        let env = get_env(db_path)?;
+
+        let block_header_db = env.create_db(Some("block_header"), DatabaseFlags::default())?;
+        let block_metadata_db = env.create_db(Some("block_metadata"), DatabaseFlags::default())?;
+        let block_body_db = env.create_db(Some("block_body"), DatabaseFlags::default())?;
+
+        let (header_hashes, body_hashes) =
+            delete_headers_and_get_hashes(block_header_db, &env, block_height)?;
+
+        let deleted_bodies = delete_from_db(block_body_db, &env, body_hashes)?;
+        let deleted_metas = delete_from_db(block_metadata_db, &env, header_hashes.clone())?;
+
+        println!(
+            "Deleted: {} block headers, {} block bodies and {} block metas",
+            header_hashes.len(),
+            deleted_bodies,
+            deleted_metas
+        );
+
+        Ok(())
+    }
+
+    fn delete_headers_and_get_hashes(
+        db: Database,
+        env: &Environment,
+        max_block_height: u64,
+    ) -> Result<(Hashes, Hashes), ToolError> {
+        let mut txn = env.begin_rw_txn()?;
+        let mut cursor = txn.open_rw_cursor(db)?;
+
+        let mut header_hashes = HashSet::new();
+        let mut body_hashes = HashSet::new();
+
+        for (raw_key, raw_val) in cursor.iter() {
+            let block_header: BlockHeader =
+                bincode::deserialize(raw_val).expect("failed to deserialize block header");
+            let block_height = block_header.height();
+
+            if block_height > max_block_height {
+                header_hashes.insert(raw_key.to_vec());
+                body_hashes.insert(block_header.body_hash.clone());
+
+                cursor.del(WriteFlags::empty())?;
+            }
+        }
+
+        drop(cursor);
+        txn.commit().expect("failed to commit transaction");
+
+        Ok((header_hashes, body_hashes))
+    }
+
+    fn delete_from_db(db: Database, env: &Environment, hashes: Hashes) -> Result<u64, ToolError> {
+        let mut txn = env.begin_rw_txn()?;
+        let mut cursor = txn.open_rw_cursor(db)?;
+        let mut count = 0;
+
+        for (raw_key, _) in cursor.iter() {
+            if hashes.contains(raw_key) {
+                cursor.del(WriteFlags::empty())?;
+                count += 1;
+            }
+        }
+
+        Ok(count)
+    }
+}
+
+pub use block::BlockHeader;
---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use clap::{App, Arg};
+
+use lmdb_utils::{db, error::ToolError};
+
+fn main() -> Result<(), ToolError> {
+    let matches = App::new("lmdb-util")
+        .arg(
+            Arg::new("db-path")
+                .required(true)
+                .short('d')
+                .long("db-path")
+                .takes_value(true)
+                .value_name("DB_PATH")
+                .help("Path to the storage.lmdb file"),
+        )
+        .arg(
+            Arg::new("block-height")
+                .required(true)
+                .short('b')
+                .long("block-height")
+                .takes_value(true)
+                .value_name("BLOCK_HEIGHT")
+                .help("blocks above this height will be deleted"),
+        )
+        .get_matches();
+
+    let path = matches.value_of("db-path").unwrap();
+    let block_height = matches
+        .value_of("block-height")
+        .unwrap()
+        .parse::<u64>()
+        .unwrap();
+
+    db::run(path.into(), block_height)
+}
