cbc8ce64b74f3995f0c249b45d9c52a57eaf863d
George Pisaltu <georgep@casperlabs.io>
2022-07-14 15:33:21 +0300

    Add util for counting db entries
    Signed-off-by: George Pisaltu <georgep@casperlabs.io>

---
+++ src/common/lmdb_utils.rs
@@ -0,0 +1,82 @@
+use std::result::Result;
+
+use lmdb::{Database, Error, Transaction};
+use lmdb_sys::{mdb_stat, MDB_stat};
+
+/// Retrieves the number of entries in a database.
+#[allow(unused)]
+pub fn entries_count<'txn, T: Transaction>(txn: &'txn T, database: Database) -> Result<usize, Error> {
+    unsafe {
+        let mut stat = MDB_stat {
+            ms_psize: 0,
+            ms_depth: 0,
+            ms_branch_pages: 0,
+            ms_leaf_pages: 0,
+            ms_overflow_pages: 0,
+            ms_entries: 0,
+        };
+        let result = mdb_stat(txn.txn(), database.dbi(), &mut stat as *mut MDB_stat);
+        if result != 0 {
+            Err(Error::from_err_code(result))
+        } else {
+            Ok(stat.ms_entries)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use lmdb::{Transaction, WriteFlags};
+
+    use crate::test_utils::LmdbTestFixture;
+
+    use super::entries_count;
+
+    #[test]
+    fn db_entries_count() {
+        let fixture = LmdbTestFixture::new(None, None);
+        let env = &fixture.env;
+        let db = fixture.db;
+
+        if let Ok(mut txn) = env.begin_ro_txn() {
+            assert_eq!(entries_count(&mut txn, db).unwrap(), 0);
+            txn.commit().unwrap();
+        }
+
+        let first_dummy_input = [0u8, 1u8];
+        let second_dummy_input = [1u8, 2u8];
+        // Insert the first entry into the database.
+        if let Ok(mut txn) = env.begin_rw_txn() {
+            txn.put(
+                fixture.db,
+                &first_dummy_input,
+                &first_dummy_input,
+                WriteFlags::empty(),
+            )
+            .unwrap();
+            txn.commit().unwrap();
+        };
+
+        if let Ok(mut txn) = env.begin_ro_txn() {
+            assert_eq!(entries_count(&mut txn, db).unwrap(), 1);
+            txn.commit().unwrap();
+        }
+
+        // Insert the second entry into the database.
+        if let Ok(mut txn) = env.begin_rw_txn() {
+            txn.put(
+                fixture.db,
+                &second_dummy_input,
+                &second_dummy_input,
+                WriteFlags::empty(),
+            )
+            .unwrap();
+            txn.commit().unwrap();
+        };
+
+        if let Ok(mut txn) = env.begin_ro_txn() {
+            assert_eq!(entries_count(&mut txn, db).unwrap(), 2);
+            txn.commit().unwrap();
+        };
+    }
+}
